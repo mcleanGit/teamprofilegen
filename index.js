e281f9884d014733e07394e0ce89f8b62837f088
Don McLean <mclean@utoronto.ca>
2022-03-04 23:04:01 -0500

    initial js including inquirer prompts

---
+++ index.js
@@ -0,0 +1,136 @@
+// index.js main JS file for teamprofilegen MyTeam app
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// need to add helper functions to dist template-literal HTML
+
+const MyTeam = [];
+
+function start() {
+ managerInput();
+}
+
+function managerInput() {
+  inquirer.prompt(
+   [
+  {
+   type: 'input',
+   name: 'name',
+   message: 'What is the Team Manager`s name?:'
+  },
+  {
+   type: "input",
+   name: "id",
+   message: 'Enter the Team Manager`s ID number:'
+  },
+  {
+   type: 'input',
+   name: 'email',
+   message: 'Team Manager`s email address:'
+  },
+  {
+   type: 'input',
+   name: 'officeNumber',
+   message: 'Team Manager`s office number:'
+  }
+ ]).then(data => {
+  const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+  console.log(manager);
+  MyTeam.push(manager);
+  addToTeam();
+ })
+};
+
+// provide function to add members to team, continuing after manager with engineer and intern prompt inputs
+function addToTeam() {
+ inquirer.prompt(
+  [
+   { 
+    type: 'list',
+    name: 'what-role',
+    message: 'Add an engineer or an intern to the team?:',
+    choices: ['Engineer', 'Intern', 'return to menu']
+   }]).then(data => {
+    if (data.what-role === 'Engineer') {
+     engineerInput();
+    } else if 
+    (data.what-role === 'Intern') {
+     internInput();
+    } else {
+     writeFile();
+    }
+  })
+}
+
+function engineerInput() {
+ inquirer.prompt(
+  [
+   { 
+    type: 'input',
+    name: 'name',
+    message: 'What is this Engineer`s name?:'
+   },
+   {
+    type: 'input',
+    name: 'id',
+    message: 'What is this Engineer`s ID number?:'
+   },
+   {
+    type: 'input',
+    name: 'email',
+    message: 'Engineer`s email address:'
+   },
+   {
+    type: 'input',
+    name: 'github',
+    message: 'What is this Engineer`s GitHub userName:'
+   }
+  ]).then(data => {
+   const engineer = new Engineer(data.name, data.id, data.email, data.github);
+   console.log(engineer);
+   MyTeam.push(engineer);
+   addToTeam();
+  })
+ };
+ 
+ function internInput() {
+  inquirer.prompt(
+   [
+    { 
+     type: 'input',
+     name: 'name',
+     message: 'What is this Intern`s name?:'
+    },
+    {
+     type: 'input',
+     name: 'id',
+     message: 'What is this Intern`s ID number?:'
+    },
+    {
+     type: 'input',
+     name: 'email',
+     message: 'Intern`s email address:'
+    },
+    {
+     type: 'input',
+     name: 'school',
+     message: 'What is the school affiliation of this Intern?:'
+    }
+   ]).then(data => {
+    const intern = new Intern(data.name, data.id, data.email, data.school);
+    console.log(intern);
+    MyTeam.push(intern);
+    addToTeam();
+   })
+  };
+
+function writeFile() {
+ fs.writeFileSync(outputPath, render(MyTeam), 'UTF-8');
+}
+
+// start invoked at end of js file
+
+start();
